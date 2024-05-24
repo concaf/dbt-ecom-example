407306345603b9ad8f0227270c0ca9dea4dc6122
Shubham <concaf@redhat.com>
2024-05-24 16:09:46 +0530

    Games by region model

---
+++ video_sales/models/games_by_region.sql
@@ -0,0 +1,72 @@
+with na_ranking as (
+    select top 1
+        name,
+        platform,
+        year,
+        genre,
+        publisher,
+        'north america' as region,
+        na_sales as sales
+    from pc_fivetran_db.video_sales_schema.video_sales_table
+    order by na_sales desc
+),
+jp_ranking as (
+    select top 1
+        name,
+        platform,
+        year,
+        genre,
+        publisher,
+        'japan' as region,
+        jp_sales as sales
+    from pc_fivetran_db.video_sales_schema.video_sales_table
+    order by jp_sales desc
+),
+eu_ranking as (
+    select top 1
+        name,
+        platform,
+        year,
+        genre,
+        publisher,
+        'europe' as region,
+        eu_sales as sales
+    from pc_fivetran_db.video_sales_schema.video_sales_table
+    order by eu_sales desc
+),
+other_ranking as (
+    select top 1
+        name,
+        platform,
+        year,
+        genre,
+        publisher,
+        'other' as region,
+        other_sales as sales
+    from pc_fivetran_db.video_sales_schema.video_sales_table
+    order by other_sales desc
+),
+global_ranking as (
+    select top 1
+        name,
+        platform,
+        year,
+        genre,
+        publisher,
+        'global' as region,
+        global_sales as sales
+    from pc_fivetran_db.video_sales_schema.video_sales_table
+    order by global_sales desc
+),
+final as (
+    select
+        *
+    from na_ranking
+    full outer join jp_ranking on 1=2
+)
+select * from na_ranking union all
+select * from jp_ranking union all
+select * from eu_ranking union all
+select * from other_ranking union all
+select * from global_ranking;
+
